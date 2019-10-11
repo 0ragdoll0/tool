0a5b017489eda28d327357ad13fea70b3f9b2782
yangxin <yangxin@rd.netease.com>
2019-10-11 14:17:14 +0800

    componentCreate tool

---
+++ README.md
@@ -0,0 +1,5 @@
+## 说明
+### 1.componentCreate
+* 功能：根据模板生成vue组件文件夹
+* 使用方法：运行componentCreate/index.ts，输入组件名称
+* 修改模板：修改componentCreate/template中的文件即可
---
+++ componentCreate/template/ComponentName.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+  </div>
+</template>
+<script lang="ts" src="./ComponentName.comp.ts"></script>
+<style lang="scss" scoped src="./ComponentName.comp.scss"></style>
+
